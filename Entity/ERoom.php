a941400bb59ee2b1d6589dd6868986194c99c560
sivi0115 <sidigiu01@gmail.com>
2025-04-09 13:06:37 +0200

    Creazione della classe ERoom
    Modifica della  classe EReply e revisione step4

---
+++ Entity/ERoom.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Entity;
+
+use InvalidArgumentException;
+
+/**
+ * Class ERoom
+ * Represents a room entity that extends the EPArea class.
+ */
+class ERoom extends EArea
+{
+    /**
+     * @var ?int Room ID, can be null if not assigned.
+     */
+    private ?int $idRoom;
+
+    /**
+     * @var float Tax (price) for reserve the room.
+     */
+    private string $tax;
+
+    /**
+     * EUser constructor.
+     *
+     * @param ?int $idRoom Room ID, can be null.
+     * @param string $areaName room's name
+     * @param int $maxGuests max number of guests (can't be negative).
+     * @throws InvalidArgumentException if one of the values not corretct
+     */
+    public function __construct(
+        ?int $idRoom,
+        string $areaName,
+        int $maxGuests,
+        int $tax
+    ) {
+        if ($tax < 0) {
+            throw new InvalidArgumentException("Il prezzo della tassa non può essere negativo.");
+        }
+        parent::__construct( $areaName, $maxGuests);
+        $this->idRoom = $idRoom;
+        $this->tax = $tax;
+    }
+
+    /**
+     * Gets the room ID.
+     *
+     * @return ?int Room ID or null if not assigned.
+     */
+    public function getIdRoom(): ?int
+    {
+        return $this->idRoom;
+    }
+
+    /**
+     * Sets the room ID.
+     *
+     * @param ?int $idRoom Room ID to set, can be null.
+     */
+    public function setIdRoom(?int $idRoom): void
+    {
+        $this->idRoom = $idRoom;
+    }
+
+    /**
+     * Get the price of the tax.
+     *
+     * @return float The price of the tax.
+     */
+    public function getTax(): float
+    {
+        return $this->tax;
+    }
+
+    /**
+     * Set th price of the tax.
+     *
+     * @param float $tax The price of the tax.
+     */
+    public function setTax(float $tax): void
+    {
+        $this->tax = $tax;
+    }
+
+    /**
+     * Serializes the object for JSON encoding.
+     *
+     * @return array The JSON-serializable data.
+     */
+    public function jsonSerialize(): array
+    {
+        return [
+            'idRoom' => $this->idRoom,
+            'name' => $this->getareaName(),
+            'maxGuests' => $this->getMaxGuests(),
+            'tax' => $this->getTax(),
+        ];
+    }
+}
