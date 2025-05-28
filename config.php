46264a19c47040d7843cca3e76a730ad0b960150
Marco <marcociprianituna2000@gmail.com>
2025-05-28 15:44:54 +0200

    inclusione classi Utility

--- config.php
+++ config.php
@@ -1,8 +1,12 @@
 <?php
 
-// Database configuration
+//Database configuration
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'ilritrovo');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
+
+
+//Session coockie expiration
+define('COOKIE_EXP_TIME', 2592000); // 30 days in seconds
 
