9ee13dc09fb45720534778a6065b359516f30887
sivi0115 <sidigiu01@gmail.com>
2025-06-05 17:05:41 +0200

    mi sparo a breve

---
+++ src/View/VUser.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__ . '/../../../vendor/smarty/smarty/libs/Smarty.class.php';
+
+class VUser {
+
+    public function showProfile($utente, $edit_section = null) {
+        $smarty = new Smarty();
+
+        // Assegna variabili Smarty
+        $smarty->assign('username', $utente->getUsername());
+        $smarty->assign('email', $utente->getEmail());
+        $smarty->assign('name', $utente->getName());
+        $smarty->assign('surname', $utente->getSurname());
+        $smarty->assign('birthdate', $utente->getBirthDate());
+        $smarty->assign('phone', $utente->getPhone());
+        $smarty->assign('edit_section', $edit_section);
+
+        // Mostra il template
+        $smarty->display('user/profile.tpl');
+    }
+}
